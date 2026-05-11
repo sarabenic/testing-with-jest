7aa7707d911201ec0f60ec95262e7382ff151a75
sarabenic <Benicsara@gmail.com>
2026-05-11 23:47:55 +0200

    La till ett test som failar

--- tests/stack.test.js
+++ tests/stack.test.js
@@ -17,3 +17,9 @@ test('peek on stack with two or more elements returns the top element', () => {
     expect(stack.peek()).toBeDefined();
     expect(stack.peek()).toBe(42);
 });
+
+test('pop should return the latest added value', () => {
+    stack.push(10);
+    stack.push(20);
+    expect(stack.pop()).toBe(10);
+});
